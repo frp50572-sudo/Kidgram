d1fbd70d6ef5b173fcd8fe1b6c5b618d8ad4c9fc
frp50572-sudo <frp50572@gmail.com>
2025-12-18 14:23:20 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+// لجعل التطبيق يعمل كـ PWA
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('messenger-v1').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
